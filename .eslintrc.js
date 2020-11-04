53ffdd3766c1decdfb681c1e9063e3354eefab7e
Luda <luda50177@gmail.com>
2020-11-04 18:56:43 +0200

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,17 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true
-	},
-	parser: 'babel-eslint',
-	parserOptions: {
-		ecmaVersion: 12
-	},
-	extends: ['@stripped-ui']
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 12
+    },
+    // extends: ['@stripped-ui'],
+
+    rules: {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "windows"],
+    },
 };
